27a430b2a8550c1c28094d49d90873dc7800e2fa
nedimfejzic <fejzic.nedim@outlook.com>
2022-03-13 01:05:17 +0100

    added a user button and registration form

---
+++ Blazor/Shared/UserRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Shared
+{
+    public class UserRegister
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(100, MinimumLength = 5)]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage ="Passwords do not match!")]
+        public string PasswordConfirm { get; set; } = string.Empty;
+
+
+    }
+}
